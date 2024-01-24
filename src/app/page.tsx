e8717abccae2efd96a2a8d880731ae858dfa50b2
Brian Kinyua <briankinyua0101@gmail.com>
2024-01-24 22:50:08 +0300

    push code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,34 @@
 "use client";
+
+import { supabase } from "@/lib/supabase";
+import { useEffect } from "react";
+
 export default function Home() {
+  const getSession = async () => {
+    const {
+      data: { session },
+    } = await supabase.auth.getSession();
+    console.log(session);
+    return session;
+  };
+  const setSession = async (access_token: string, refresh_token: string) => {
+    const { data, error } = await supabase.auth.setSession({
+      access_token,
+      refresh_token,
+    });
+    console.log(data);
+    console.log(error);
+    return true;
+  };
+  const refreshSession = async () => {
+    const {
+      data: { session },
+    } = await supabase.auth.refreshSession();
+    console.log(session);
+    return session;
+  };
+  useEffect(() => {
+    getSession();
+  }, []);
   return <div>Logged in</div>;
 }
