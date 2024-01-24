696c20238f2f5ccdec5fc3f49322aa782e426803
Brian Kinyua <briankinyua0101@gmail.com>
2024-01-24 21:05:05 +0300

    done first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
-
+import { supabase } from "@/lib/supabase";
 
 export default function Home() {
- 
+  const setNewView = async () => {
+    const { data, error } = await supabase.from("views").insert({
+      name: "random name",
+    });
 
-  return (
-    <div>
-        Hello
-    </div>
-  )
+    if (data) console.log(data);
+    if (error) console.log(error);
+  };
+
+  setNewView();
+
+  return <div>hello</div>;
 }
